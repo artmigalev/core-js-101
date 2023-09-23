83358048dfc70d39047ee48bb0619c33f5924d72
david-anav <art_migalev@yahoo.com>
2023-09-23 14:26:38 +0300

    feat:  1. 15/14

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,31 +1,42 @@
-const Uplitte = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const Lowlitter = 'abcdefghijklmnopqrstuvwxyz'.split('');
-function encodeToRot13(str) {
-  const rot = [];
-  let k;
-  for (const i of str) {
-    if (i.startsWith(' ') || i.startsWith('!') || i.startsWith('?') || i.startsWith('.') || i.startsWith(',')) {
-      rot.push(i);
-      continue;
-    }
-    if (i === i.toLowerCase()) {
-      k = Lowlitter.indexOf(i) + 13;
+// const Uplitte = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+// const Lowlitter = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// function encodeToRot13(str) {
+//   const rot = [];
+//   let k;
+//   for (const i of str) {
+//     if (i.startsWith(' ') || i.startsWith('!') || i.startsWith('?') || i.startsWith('.') || i.startsWith(',')) {
+//       rot.push(i);
+//       continue;
+//     }
+//     if (i === i.toLowerCase()) {
+//       k = Lowlitter.indexOf(i) + 13;
 
-      if (Number(Lowlitter.length - 1) >= k) {
-        rot.push(Lowlitter[k]);
-      } else {
-        rot.push(Lowlitter[k - Number(Lowlitter.length - 1) - 1]);
-      }
-    } else if (i === i.toUpperCase()) {
-      k = Uplitte.indexOf(i) + 13;
+//       if (Number(Lowlitter.length - 1) >= k) {
+//         rot.push(Lowlitter[k]);
+//       } else {
+//         rot.push(Lowlitter[k - Number(Lowlitter.length - 1) - 1]);
+//       }
+//     } else if (i === i.toUpperCase()) {
+//       k = Uplitte.indexOf(i) + 13;
 
-      if (Number(Uplitte.length - 1) >= k) {
-        rot.push(Uplitte[k]);
-      } else {
-        rot.push(Uplitte[k - Number(Uplitte.length - 1) - 1]);
-      }
-    }
-  }
-  return rot.join('');
+//       if (Number(Uplitte.length - 1) >= k) {
+//         rot.push(Uplitte[k]);
+//       } else {
+//         rot.push(Uplitte[k - Number(Uplitte.length - 1) - 1]);
+//       }
+//     }
+//   }
+//   return rot.join('');
+// }
+// console.log(encodeToRot13('hello'));
+
+
+function getCardId(value) {
+  const log = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  return Number(log.indexOf(value));
 }
-console.log(encodeToRot13('hello'));
+
+console.log(getCardId('6♣'))
+
+
+
--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -277,8 +277,10 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+
+function getCardId(value) {
+  const log = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  return Number(log.indexOf(value));
 }
 
 module.exports = {
