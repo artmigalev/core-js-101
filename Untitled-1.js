a3ce0741d3087f2a5b18cce99ba0a3cf1dd20e0a
david-anav <art_migalev@yahoo.com>
2023-10-12 17:47:05 +0300

    feat: 38/38

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,12 +1,11 @@
-function getIdentityMatrix(n) {
-  let i = 0;
-  let arr =[...Array(n)].map(() => {return Array(n).fill(0);});
-  return arr.map((elem) => {
-    elem.splice(i,1,1)
-    i+=1
-    return elem
-  })
+function generateOdds(len) {
+  var n = -1;
+  const arr = Array(len).fill(1);
+  return arr.map(() => {
 
+    return n += 2; });
 }
-console.log(getIdentityMatrix(5))
+
+console.log(generateOdds(1))
+
 
--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -34,8 +34,13 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let n = -1;
+  const arr = Array(len).fill(1);
+  return arr.map(() => {
+    n += 2;
+    return n;
+  });
 }
 
 /**
