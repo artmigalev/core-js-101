f832d769f0c3b3324c5884381c9adf52f7980445
artmigalev <art_migalev@yahoo.com>
2024-10-07 19:16:24 +0300

    11 /6

--- TEST.js
+++ TEST.js
@@ -1,4 +1,22 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
+function isCreditCardNumber(ccn) {
+  const controlNum = Number(
+    ccn.toString().slice(ccn.toString().length - 1, ccn.toString().length)
+  );
+  let second = true;
+  const digits = ccn.toString().split('');
+  let sum = 0;
+  for (let ind = digits.length - 2; ind >= 0; ind--) {
+    let digit = Number(digits[ind]);
+    if (second) {
+      digit *= 2;
+      if (digit > 9) {
+        digit = Math.trunc(digit / 10) + (digit % 10);
+      }
+    }
+    sum += digit;
+    second = !second;
+  }
+  console.log(sum, (10 - (sum % 10)) % 10 === controlNum);
 }
-reverseString('The quick brown fox jumps over the lazy dog');
+
+isCreditCardNumber(54891243456789010);
