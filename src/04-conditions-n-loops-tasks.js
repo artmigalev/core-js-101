d6c2eb545be1ba791452206559714d323368df40
artmigalev <art_migalev@yahoo.com>
2024-10-08 12:43:46 +0300

    12/6

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -312,8 +312,10 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const numArray = Array.from(String(num), Number);
+  const sum = numArray.reduce((acc, curr) => acc + curr, 0);
+  return sum <= 9 ? sum : getDigitalRoot(sum);
 }
 
 /**
